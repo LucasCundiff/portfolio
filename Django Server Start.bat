9915adc47a8f246e90fc582268594fab3ed15758
Lucas Cundiff <140455663+LucasCundiff@users.noreply.github.com>
2026-02-12 16:13:48 -0500

    Removed AWS Features

---
+++ Django Server Start.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd C:\Users\lucas\Desktop\Job Search\portfolio\scripts && call activate.bat && cd C:\Users\lucas\Desktop\Job Search\portfolio\portfolio"
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': rds_name,
-        'USER': rds_username,
-        'PASSWORD': rds_password,
-        'HOST': rds_host,
-        'PORT': rds_port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
