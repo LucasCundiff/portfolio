3de4671fd5d71c1884816f6ef292698b5ab34ce7
Lucas Cundiff <140455663+LucasCundiff@users.noreply.github.com>
2026-02-13 00:59:18 -0500

    Porfolio Updated
    Updated portfolio to now only show projects instead of skills as well

--- portfolio/myPortfolio/models.py
+++ portfolio/myPortfolio/models.py
@@ -1,25 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class Skill(models.Model):
-    Frontend = "Front End"
-    Backend = "Back End"
-    Database = "Database"
-    ToolTech = "ToolTech"
-    Choices = {
-        Frontend : "Front End",
-        Backend : "Back End",
-        Database : "Database",
-        ToolTech: "ToolTech",
-    }
-
-    skillSet = models.CharField(max_length=255, choices=Choices, default=Frontend)
-    skillName = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return str(self.skillSet) + " Skill: " + str(self.skillName)
-
-
 class Project(models.Model):
     projectName = models.CharField(max_length=255)
     projectLink = models.URLField()
--- portfolio/myPortfolio/views.py
+++ portfolio/myPortfolio/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Skill
 from .models import Project
 
 # Create your views here.
@@ -10,10 +9,6 @@ def main(request):
     template = loader.get_template("main.html")
 
     context = {
-        "frontEndSkills" : Skill.objects.filter(skillSet="Front End"),
-        "backEndSkills" : Skill.objects.filter(skillSet="Back End"),
-        "databaseSkills" : Skill.objects.filter(skillSet="Database"),
-        "toolTechSkills" : Skill.objects.filter(skillSet="ToolTech"),
         "featuredProjects" : Project.objects.filter(projectFeatured=True),
         "extraProjects" : Project.objects.filter(projectFeatured=False)
     }
